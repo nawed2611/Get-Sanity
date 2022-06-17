a4050737a19c1e80cae6d35bd2c1595848aabacf
Nawed Ali <nawed2611@gmail.com>
2022-06-18 02:12:58 +0530

    Add axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
+import {useState} from 'react';
+
 function App() {
 
+  // const
+
   return (
-    <div className="App">
-        <h1 className="text-3xl font-bold mx-auto">Hello World</h1>
+    <div className="flex flex-col h-screen">
+        <h1 className="p-2 m-2 flex flex-col align-center justify-center text-3xl font-bold mx-auto">How are you feeling today?</h1>
+        <form className="p-2 m-2 flex flex-col h-3/4 align-center justify-center text-xl">
+          <input type="text" className="border-2 border-black" placeholder="" />
+          <button>Send</button>
+        </form>
     </div>
   )
 }
