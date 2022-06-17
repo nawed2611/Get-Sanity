7fbb6bbd865a3deda125cda6fa441ad5a307c74d
Mohammad Arshad <mdarshad1000@gmail.com>
2022-06-18 03:09:46 +0530

    API working

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,8 @@
 from tracemalloc import reset_peak
-from flask import Flask
-import json
+from flask import Flask, request
 import paralleldots
 
-
 app = Flask(__name__)
-
 
 paralleldots.set_api_key("bwQrWhxsrnOZS57WuO93Ohowv6giQt7EPBTwOhtm2BI")
 
@@ -13,15 +10,21 @@ paralleldots.set_api_key("bwQrWhxsrnOZS57WuO93Ohowv6giQt7EPBTwOhtm2BI")
 @app.route('/')
 def home():
 
+    if request.method == 'POST':
 
-    # for single sentence
-    text="you are a motherfucker"
-    lang_code="en"
-    response=paralleldots.sentiment(text,lang_code)
-    # data = json.loads(response)
-    # print(type(data))
-    return response
+        # for single sentence
+        text = request.form.get('title')
+        lang_code = "en"
+        response = paralleldots.sentiment(text, lang_code)
+
+        # converting nested dictionary to simple dictionary
+        modified_response = response['sentiment']
+
+        # finding max value from positive, negative, neutral
+        mood_type = max(modified_response, key=lambda x: modified_response[x])
+
+        return mood_type
 
 
 
-app.run(debug=True)
+app.run(debug=True, host='0.0.0.0', port=80)
