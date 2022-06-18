7fe1bdd0a2f7e3555f6e1d62e15f95b5aadc6f80
Mohammad Arshad <mdarshad1000@gmail.com>
2022-06-19 04:10:11 +0530

    Integrate model

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,9 @@ from tracemalloc import reset_peak
 from flask import Flask, request, render_template
 import paralleldots
 from flask_cors import CORS, cross_origin
+import pickle
+import os
+
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -29,6 +32,25 @@ def home():
         return {"mood": mood_type}
     else:
         return {"mood": "no response"}
+
+
+@app.route('/survey')
+def model():
+    pickled_model = pickle.load(open("//Users//arshad//Desktop//Projects//mlh-mental-health-hacks//model//knn_model.pkl", 'rb'))
+    temp = pickled_model.predict([[0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0]])
+    next = list(temp)
+    answer = str(next[0])
+
+    if answer == '0':
+        return 'Normal'
+    elif answer == '1':
+        return 'Loneliness'
+    elif answer == '2':
+        return 'Anxiety'
+    elif answer == '3':
+        return 'Stress'
+    elif answer == '4':
+        return "Depression"
 
 
 app.run(debug=True)
