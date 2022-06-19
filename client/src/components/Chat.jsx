e47caed4c30be4d42d14b9e5b7262df074f99854
Nawed Ali <nawed2611@gmail.com>
2022-06-19 17:53:44 +0530

    Add chat

---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  
+
+const Chat = ({open, setOpen}) => {
+
+    const handleClose = () => setOpen(false);
+
+    return (
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Hey! I am Marv
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?
+                </Typography>
+            </Box>
+        </Modal>
+    )
+}
+
+export default Chat
