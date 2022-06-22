fd6420a31be6a6217684c972860cb6ebcf7de0e2
Nawed Ali <nawed2611@gmail.com>
2022-06-23 03:17:43 +0530

    Add quotes

---
+++ client/src/components/Quotes.js
@@ -0,0 +1,24 @@
+const quotes = [
+    "When you have a dream, you've got to grab it and never let go",
+    "Nothing is impossible.",
+    "Don't settle for average. Bring your best to the moment. Then, whether it fails or succeeds, at least you know you gave all you had.",
+    "Show up, show up, show up, and after a while the muse shows up, too.",
+    "Don't bunt. Aim out of the ballpark. Aim for the company of immortals.",
+    "I have stood on a mountain of no’s for one yes.",
+    "If you believe something needs to exist, if it's something you want to use yourself, don't let anyone ever stop you from doing it.",
+    "The battles that count aren't the ones for gold medals. The struggles within yourself—the invisible, inevitable battles inside all of us—that's where it's at.",
+    "If there is no struggle, there is no progress.",
+    "Courage is like a muscle. We strengthen it by use.",
+    "Develop success from failures. Discouragement and failure are two of the surest stepping stones to success",
+    "Relentlessly prune bullshit, don't wait to do things that matter, and savor the time you have. That's what you do when life is short.",
+    "More is lost by indecision than wrong decision.",
+    "If the highest aim of a captain were to preserve his ship, he would keep it in port forever..",
+    "You can be the ripest, juiciest peach in the world, and there's still going to be somebody who hates peaches.",
+    "Keep a little fire burning; however small, however hidden.",
+    "Nothing in the world can take the place of Persistence. Talent will not; nothing is more common than unsuccessful men with talent. Genius will not; unrewarded genius is almost a proverb. Education will not; the world is full of educated derelicts. The slogan 'Press On' has solved and always will solve the problems of the human race.",
+    "Worry is a misuse of imagination",
+    "Courage is the most important of all the virtues because without courage, you can't practice any other virtue consistently.",
+    "Somewhere, something incredible is waiting to be known"
+  ];
+
+export default quotes
