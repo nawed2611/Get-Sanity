6eab1a624d1371a499a54ebcf6d2dc4fd09f6d14
Nawed Ali <nawed2611@gmail.com>
2022-06-23 03:34:34 +0530

    Minor bug changes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -33,7 +33,6 @@ const Home = () => {
 
       <div className="flex flex-col justify-center items-center homediv">
 
-        // Displaying User's name using Auth0
         {isAuthenticated && (<Typography variant="h4" className="font-bold">Hi, {user.name}</Typography>)}
 
         <Typography variant="h1" className="font-bold text-4xl m-1 p-2">How are you feeling today?</Typography>
@@ -41,11 +40,11 @@ const Home = () => {
         <form onSubmit={handleSubmit} className="p-2 m-2 flex flex-col w-1/2 h-1/4 items-center justify-center">
           <Input type="text" value={text} onChange={(e) => { setText(e.target.value) }} className="font-bold text-4xl m-2 p-2 w-full h-1/2" placeholder="Like how was your day..." />
         </form>
-        
+
       </div>
       {result && <Typography className="m-2 p-2 flex items-center justify-center h-40" variant="h3">Scroll Down, We have something for you ↓↓</Typography>}
 
-      // Displaying Playlist component when mood is positive
+      
       {
         result && result["mood"] == 'positive' && (
           <div className="m-2 p-2 flex flex-col items-center border border-black justify-center">
@@ -53,7 +52,7 @@ const Home = () => {
           </div>
         )
       }
-      // Displaying Form component when mood is negative
+
       {
         result && result["mood"] == 'negative' && (
           <Form />)
