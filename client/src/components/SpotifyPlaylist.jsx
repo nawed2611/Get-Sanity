f53312c30f6abd68f9c27caf12585a99f1ae18b5
Nawed Ali <nawed2611@gmail.com>
2022-06-23 03:17:36 +0530

    Add playlist

--- client/src/components/SpotifyPlaylist.jsx
+++ client/src/components/SpotifyPlaylist.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 const SpotifyPlaylist = () => {
     return (
-        <Stack className="font-bold flex flex-col items-center justify-center">
-            <Typography variant="h2">Yayy! Here are some Happy Hits that you might wanna listen</Typography>
-            <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DXdPec7aLTmlC?utm_source=generator" width="40%" height="480" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-        </Stack>
+        <Box className="flex items-center justify-center m-2 p-2">
+            <Typography variant="h4" className='m-2 p-2'>Yayy! Here are some Spotify Playlists that we found to help you conquer your day</Typography>
+            <iframe className='m-2 p-2 rounded-md' src="https://open.spotify.com/embed/playlist/37i9dQZF1DXdPec7aLTmlC?utm_source=generator" width="40%" height="480" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            <iframe className='m-2 p-2' src="https://open.spotify.com/embed/playlist/7yc76d3B7ClVnx5gFeIcQO?utm_source=generator" width="40%" height="480" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        </Box>
     )
 }
 
